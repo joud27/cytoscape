18184fd17a350ad341478f03c684d93a29d7b8f0
Areej <arij.alkharrat@bordeauxuniversity.fr>
2023-04-23 17:21:21 +0200

    new design with ornasim

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+var conditions = [];
+var p_values = [];
+
 document.getElementById('json_input_file').addEventListener('change', function (e) {
     if (e.target.files[0]) {
 
@@ -7,14 +10,25 @@ document.getElementById('json_input_file').addEventListener('change', function (
         reader.readAsText(file, "UTF-8");
         reader.onload = function (evt) {
             let all_networks = JSON.parse(evt.target.result);
-
+            console.log(all_networks);
             process_data(all_networks);
+            // clustering(all_networks);
+
+            for (var i = 0; i < conditions.length; i++) {
+                addCondition(conditions[i], "conditions");
+            }
+
+            for (var i = 0; i < p_values.length; i++) {
+                addCondition(p_values[i], "p_values");
+            }
         }
         reader.onerror = function (evt) {
             console.log("error reading file");
         }
     }
 });
+
+
 
 
 function process_data(allNetwork) {
@@ -24,18 +38,21 @@ function process_data(allNetwork) {
 
     for (var i = 0; i < nodes.length; i++) {
         let node_data = nodes[i].data;
+        let node_position = nodes[i].position;
 
         let node_data_id = node_data.id;
 
-        if (nodes[i].position) {
+        if (node_position) {
             let node_data_uniprot = node_data.uniprot;
-            let node_data_log_ratio = node_data.log_ratio;
+            // let node_data_log_ratio = node_data.log_ratio;
             let node_data_description = node_data.description;
             let node_data_displayName = node_data.displayName;
             let node_data_parent = node_data.parent;
             let node_data_codeBP = node_data.codeBP;
 
-            let node_position = nodes[i].position;
+
+            getConditionsAndPValues(node_data);
+
             let node_position_x = node_position.x;
             let node_position_y = node_position.y;
 
@@ -55,5 +72,80 @@ function process_data(allNetwork) {
         let edge_data_stringdb_score = edge_data.stringdb_score;
 
 
+    }
+}
+
+function clustering(allNetwork) {
+
+    var clusters = {};
+    let nodes = allNetwork.nodes;
+
+    for (var i = 0; i < nodes.length; i++) {
+
+        if (nodes[i].position) {
+
+            let parent = nodes[i].data.parent;
+
+            if (!(parent in clusters)) {
+                clusters[parent] = [];
+            }
+
+            clusters[parent].push(nodes[i]);
+        }
+    }
+
+}
+
+function addCondition(condition, parent_id) {
+    var option = document.createElement("option");
+    option.value = condition;
+    option.innerHTML = condition;
+
+    var parentDiv = document.getElementById(parent_id);
+    parentDiv.appendChild(option);
+}
+
+function getConditionsAndPValues(data) {
+    for (key in data) {
+        if (key.indexOf("p_value") !== -1) {
+            if (!p_values.includes(key)) {
+                p_values.push(key);
+            }
+        } else if (key.indexOf("condition") !== -1) {
+            if (!conditions.includes(key)) {
+                conditions.push(key);
+            }
+        }
+    }
+}
+
+
+function filterByCondition() {
+    var allConditions = document.getElementsByClassName("conditions_item");
+    var allPValues = document.getElementsByClassName("p_values_item");
+
+    for (var i = 0; i < allConditions.length; i++) {
+        if (allConditions[i].checked) {
+
+        }
+    }
+}
+
+function changeOrganism(organism) {
+    document.getElementById("organism").value = organism;
+
+    var algo = document.getElementById("algorithm");
+    algo.innerHTML = "";
+
+    var option = document.createElement("option");
+    option.value = "abundance_group_control";
+    option.innerHTML = "abundance group control";
+    algo.appendChild(option);
+
+    if (organism == "human") {
+        var option2 = document.createElement("option");
+        option2.value = "skin_tissue";
+        option2.innerHTML = "skin tissue";
+        algo.appendChild(option2);
     }
 }
